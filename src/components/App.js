b5837b9f6bc160a8266fc5905c97d508ca595af9
Vlad Simon <25804956+neruL02@users.noreply.github.com>
2023-02-20 15:27:45 +0100

    Changed logic following the boardgame.io documentation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,21 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import GameBoard from './GameBoard';
+import { Client } from 'boardgame.io/react';
+import { Werewolf } from './Game';
+import { Board } from './Board';
+import {Local} from "boardgame.io/multiplayer";
 
-function App() {
-    // Initialize game state
-    const [players, setPlayers] = useState([
-        { name: 'Player 1', role: 'Villager', alive: true },
-        { name: 'Player 2', role: 'Villager', alive: true },
-        { name: 'Player 3', role: 'Werewolf', alive: true },
-        { name: 'Player 4', role: 'Seer', alive: true },
-        { name: 'Player 5', role: 'Doctor', alive: true },
-    ]);
-    const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
-    const [gamePhase, setGamePhase] = useState('night');
-    const [gameStatus, setGameStatus] = useState('Werewolves, open your eyes...');
+const WerewolfClient = Client({
+    game: Werewolf,
+    numPlayers: 5,
+    board: Board,
+    multiplayer: Local(),
+    debug: true,
+});
 
-    // Handler for when a player performs an action
-    function handleAction(role) {
-        // Perform action based on player's role
-        if (role === 'Werewolf') {
-            // Werewolf action logic
-        } else if (role === 'Seer') {
-            // Seer action logic
-        } else if (role === 'Doctor') {
-            // Doctor action logic
-        }
+const App = () => (
+    <div>
+        {/*for each player, create a new client*/}
 
-        // Update game state
-        // ...
-    }
-
-    return (
-        <div className="App">
-            <Header title="Werewolf Game"
-                playerName={players[currentPlayerIndex].name}
-                playerRole={players[currentPlayerIndex].role}
-                gameStatus={gameStatus}
-            />
-            <GameBoard
-                players={players}
-                currentPlayerIndex={currentPlayerIndex}
-                gamePhase={gamePhase}
-                onAction={handleAction}
-            />
-
-        </div>
-    );
-}
+    </div>
+);
 
 export default App;
