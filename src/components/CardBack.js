bd050aa4ae31fe9bc4f39d076fafd7bf11b6e55a
Vlad Simon <25804956+neruL02@users.noreply.github.com>
2023-02-20 10:55:49 +0100

    Fix card display in react. TODO: resize the image

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -1,11 +1,13 @@
 // CardBack.js
 
 import React from 'react';
+import cardBackImage from '../images/back.png';
+
 
 function CardBack() {
     return (
         <div className="card-back">
-            <img src="back.png" alt="card back" />
+            <img src={cardBackImage} alt="Card Back" />
         </div>
     );
 }
