8d165028b632468cfe61657330d0e1b3e3612a7c
Vlad Simon <25804956+neruL02@users.noreply.github.com>
2023-02-20 20:30:27 +0100

    Extended implementation of the rules. There are errors still.

--- src/Game.js
+++ src/Game.js
@@ -1,33 +1,165 @@
-import { INVALID_MOVE } from 'boardgame.io/core';
+import {INVALID_MOVE} from "boardgame.io/core";
+
 
 export const WerewolfGame = {
-    setup: (G, ctx) => ({
+    name: "werewolf",
+    setup: (ctx) => ({
         players: ctx.playOrder.map((player) => ({
             name: player,
             role: null,
+            eliminated: false,
+            votes: 0,
         })),
-        phase: 'lobby',
-        werewolfTargets: [],
-        seerTarget: null,
-        doctorTarget: null,
         eliminatedPlayer: null,
     }),
 
-    moves: {
-        startGame: (G, ctx) => {
-            if (G.players.length < 3) {
-                return INVALID_MOVE;
-            }
-            G.phase = 'night';
-        },
-
-        endNight: (G, ctx) => {
-            G.phase = 'day';
-        },
-
-        endGame: (G, ctx) => {
-            const werewolfWin = G.werewolf.length >= G.players.length / 2;
-            return { werewolfWin };
+    flow: {
+        startingPhase: "night",
+        phases: {
+            night: {
+                turn: {
+                    order: {
+                        first: (G, ctx) => {
+                            return ctx.playOrder.find(
+                                (player) => G.players[player].role === "werewolf"
+                            );
+                        },
+                        next: (G, ctx) => {
+                            const currentPlayerIndex = ctx.playOrder.indexOf(ctx.currentPlayer);
+                            const nextPlayerIndex = (currentPlayerIndex + 1) % ctx.numPlayers;
+                            const nextPlayerId = ctx.playOrder[nextPlayerIndex];
+                            const nextPlayerRole = G.players[nextPlayerId].role;
+                            if (nextPlayerRole === "seer" || nextPlayerRole === "doctor") {
+                                return nextPlayerId;
+                            }
+                            return ctx.playOrder.find(
+                                (player) => G.players[player].role === "werewolf"
+                            );
+                        },
+                    },
+                    moves: {
+                        chooseElimination: (G, ctx, playerId) => {
+                            if (G.eliminatedPlayer !== null) {
+                                return INVALID_MOVE;
+                            }
+                            G.eliminatedPlayer = playerId;
+                        },
+                        chooseInvestigation: (G, ctx, playerId) => {
+                            if (G.eliminatedPlayer !== null) {
+                                return INVALID_MOVE;
+                            }
+                            const playerRole = G.players[playerId].role;
+                            ctx.events.setActivePlayers({
+                                value: {
+                                    [playerId]: playerRole,
+                                },
+                                moveLimit: 1,
+                            });
+                        },
+                        chooseSave: (G, ctx, playerId) => {
+                            if (G.eliminatedPlayer !== null) {
+                                return INVALID_MOVE;
+                            }
+                            G.players[playerId].saved = true;
+                        },
+                    },
+                },
+                next: "day",
+            },
+            day: {
+                turn: {
+                    order: {
+                        first: (G, ctx) => {
+                            return ctx.playOrder[0];
+                        },
+                        next: (G, ctx) => {
+                            const currentPlayerIndex = ctx.playOrder.indexOf(ctx.currentPlayer);
+                            const nextPlayerIndex = (currentPlayerIndex + 1) % ctx.numPlayers;
+                            const nextPlayerId = ctx.playOrder[nextPlayerIndex];
+                            const nextPlayerRole = G.players[nextPlayerId].role;
+                            if (nextPlayerRole === "seer" || nextPlayerRole === "doctor") {
+                                return nextPlayerId;
+                            }
+                            return ctx.playOrder[nextPlayerIndex];
+                        },
+                    },
+                    moves: {
+                        accuse: {
+                            moves: {
+                                vote: (G, ctx, accusedPlayerId) => {
+                                    G.players[accusedPlayerId].votes += 1;
+                                },
+                            },
+                            next: "trial",
+                        },
+                        skip: {
+                            moves: {
+                                skipVote: (G, ctx) => {},
+                            },
+                            next: "night",
+                        },
+                    },
+                },
+                next: (G, ctx) => {
+                    const players = G.players.filter((player) => !player.eliminated);
+                    const numWerewolves = players.filter((player) => player.role === "werewolf").length;
+                    const numNonWerewolves = players.length - numWerewolves;
+                    if (numWerewolves === 0) {
+                        return "endGame";
+                    }
+                    if (numWerewolves >= numNonWerewolves) {
+                        return "endGame";
+                    }
+                    return "night";
+                },
+            },
+            trial: {
+                turn: {
+                    order: {
+                        first: (G, ctx) => {
+                            return ctx.playOrder.find(
+                                (player) => G.players[player].votes === Math.max(...G.players.map((p) => p.votes))
+                            );
+                        },
+                        next: (G, ctx) => {
+                            const currentPlayerIndex = ctx.playOrder.indexOf(ctx.currentPlayer);
+                            const nextPlayerIndex = (currentPlayerIndex + 1) % ctx.numPlayers;
+                            const nextPlayerId = ctx.playOrder[nextPlayerIndex];
+                            const nextPlayerRole = G.players[nextPlayerId].role;
+                            if (nextPlayerRole === "seer" || nextPlayerRole === "doctor") {
+                                return nextPlayerId;
+                            }
+                            return ctx.playOrder[nextPlayerIndex];
+                        },
+                    },
+                    moves: {
+                        vote: (G, ctx, accusedPlayerId) => {
+                            G.players[accusedPlayerId].votes += 1;
+                        },
+                    },
+                },
+                next: (G, ctx) => {
+                    const accusedPlayer = G.players[G.eliminatedPlayer];
+                    if (accusedPlayer.votes > (ctx.numPlayers / 2)) {
+                        accusedPlayer.eliminated = true;
+                    }
+                    G.eliminatedPlayer = null;
+                    return "day";
+                },
+            },
+            endGame: {
+                onEnd: (G, ctx) => {
+                    const werewolves = G.players.filter((player) => player.role === "werewolf" && !player.eliminated);
+                    const villagers = G.players.filter((player) => player.role !== "werewolf" && !player.eliminated);
+                    let message = "";
+                    if (werewolves.length === 0) {
+                        message = "Villagers Win!";
+                    } else {
+                        message = "Werewolves Win!";
+                    }
+                    ctx.events.endGame({ message });
+                },
+            },
         },
     },
 };
