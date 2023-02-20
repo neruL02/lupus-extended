aef499eb3ca03c56385ff246f3935c7bc16e5215
Vlad Simon <25804956+neruL02@users.noreply.github.com>
2023-02-20 19:36:28 +0100

    Fix import error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Client } from 'boardgame.io/react';
-import { WerewolfGame } from './game';
+import { WerewolfGame } from './Game';
 import { GameBoard } from './components/GameBoard';
 
 const WerewolfClient = Client({
