89eebdbd443861ea916d4b9c85685d3604215ea5
Vlad Simon <25804956+neruL02@users.noreply.github.com>
2023-02-20 12:50:41 +0100

    Missed the implementation of the game logic.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,3 +1,81 @@
-export default class Game {
+const Werewolf = {
+    setup: (ctx) => {
+        const playerRoles = assignRoles(ctx.numPlayers);
+        return {
+            players: Array(ctx.numPlayers).fill(null),
+            playerRoles,
+            revealedRoles: Array(ctx.numPlayers).fill(false),
+            votes: Array(ctx.numPlayers).fill(0),
+            phase: "day",
+            currentTurn: 0,
+        };
+    },
 
-}
+    moves: {
+        vote: (G, ctx, voteTarget) => {
+            const playerID = ctx.currentPlayer;
+            G.votes[voteTarget]++;
+            G.players[playerID].voted = true;
+            ctx.events.endTurn();
+        },
+        revealRole: (G, ctx, playerID) => {
+            G.revealedRoles[playerID] = true;
+            ctx.events.endTurn();
+        },
+    },
+
+    flow: {
+        startingPhase: "day",
+
+        phases: {
+            day: {
+                turn: {
+                    order: {
+                        first: (G, ctx) => ctx.currentPlayer,
+                        next: (G, ctx) => (ctx.currentPlayer + 1) % ctx.numPlayers,
+                    },
+                },
+                movesPerTurn: 1,
+                onTurnBegin: (G, ctx) => {
+                    G.currentTurn++;
+                    G.players[ctx.currentPlayer].voted = false;
+                    if (G.currentTurn > 1) {
+                        checkIfGameOver(G, ctx);
+                    }
+                },
+                endTurnIf: (G, ctx) => {
+                    if (G.players.every((player) => player.voted)) {
+                        return { next: "night" };
+                    }
+                },
+            },
+
+            night: {
+                turn: {
+                    order: {
+                        first: (G, ctx) => ctx.currentPlayer,
+                        next: (G, ctx) => (ctx.currentPlayer + 1) % ctx.numPlayers,
+                    },
+                },
+                movesPerTurn: 1,
+                onTurnBegin: (G, ctx) => {
+                    G.players[ctx.currentPlayer].voted = false;
+                },
+                endTurnIf: (G, ctx) => {
+                    if (G.players.every((player) => player.voted)) {
+                        return { next: "day" };
+                    }
+                },
+            },
+        },
+    },
+
+    endIf: (G, ctx) => {
+        if (G.phase === "gameover") {
+            return { winner: checkWinCondition(G) };
+        }
+    },
+};
+
+
+export default Werewolf;
